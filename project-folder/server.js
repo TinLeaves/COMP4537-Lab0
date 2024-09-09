3f40e1415792a62d0c866f38190e41f8b3a2e489
TinLeaves <tinleaves.0@gmail.com>
2024-09-08 21:35:40 -0700

    update to server.js

--- project-folder/server.js
+++ project-folder/server.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000; 
 
 app.use('/js', express.static(path.join(__dirname, 'js')));
+app.use('/lang/messages/en',express.static(path.join(__dirname,'lang/messages/en')))
 
 // Route for the home page
 app.get('/', (req, res) => {
